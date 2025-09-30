e4410a55027b333e2cf1f6a10ae27cf30a6ac49f
정민규 <114897197+m9-m9@users.noreply.github.com>
2025-09-30 14:15:59 +0900

    Time: 39 ms (72.92%), Space: 54.2 MB (15.71%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -4,14 +4,16 @@
 Array.prototype.last = function() {
     
     
-    if(this.length ===0) {
-        
+    
+    if(this.length ===0){
         return -1
-    } 
-    
+    }
+
     return this[this.length-1]
-    
+
+
 };
+
 
 /**
  * const arr = [1, 2, 3];
