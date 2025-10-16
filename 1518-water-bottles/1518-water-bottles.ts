9eb856699c9793c69e236f761440482958ebcdd5
정민규 <114897197+m9-m9@users.noreply.github.com>
2025-10-16 15:19:40 +0900

    Time: 0 ms (100%), Space: 55.1 MB (45.33%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.ts
+++ 1518-water-bottles/1518-water-bottles.ts
@@ -1,19 +1,13 @@
 function numWaterBottles(numBottles: number, numExchange: number): number {
-    // 처음 numBottles개를 모두 마심
-    let totalDrunk = numBottles;
+  
+    let totalDrunks = numBottles;
     let emptyBottles = numBottles;
     
-    // 빈 병으로 교환할 수 있는 동안 반복
-    while (emptyBottles >= numExchange) {
-        // 빈 병을 새 병으로 교환
-        const newBottles = Math.floor(emptyBottles / numExchange);
-        
-        // 새 병을 마심
-        totalDrunk += newBottles;
-        
-        // 빈 병 = 교환 후 남은 빈 병 + 방금 마신 병
-        emptyBottles = (emptyBottles % numExchange) + newBottles;
+    while(emptyBottles>=numExchange){
+
+        totalDrunks += Math.floor( emptyBottles / numExchange)
+        emptyBottles = Math.floor( emptyBottles / numExchange) + emptyBottles % numExchange
     }
     
-    return totalDrunk;
+    return totalDrunks;
 }
