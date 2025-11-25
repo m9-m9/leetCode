eac733e146dcc2b63f01d5c69171550ad4656cac
정민규 <114897197+m9-m9@users.noreply.github.com>
2025-11-25 16:55:45 +0900

    Time: 1 ms (29.71%), Space: 55.1 MB (71.13%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,16 +1,18 @@
 function strStr(haystack: string, needle: string): number {
     
+        if(haystack.length < needle.length ) return -1
 
-        if(haystack.length < needle.length) return -1
 
-        for (let i = 0; i <= haystack.length - needle.length; i++) {
-        if (haystack.slice(i, i + needle.length) === needle) return i;
+        for(let i=0; i <= haystack.length- needle.length; i++ ) {
+
+                if(needle === haystack.slice(i, i + needle.length) ){
+
+                    return i
+                } 
+
         }
-        return -1;
+
+
+        return -1
+
 };
-
-
-
-
-
-
